d839cbfd5ad79be5b68a9f9310ba445c2458a79f
Mangat Rai <mangatrai@gmail.com>
2023-10-20 12:37:07 -0500

    Added Streamlit UI

---
+++ Astra_Hybrid_Search_Bike_Recommendation/demo-ui.py
@@ -0,0 +1,129 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.query import dict_factory, SimpleStatement
+import openai, os, uuid, time, requests, traceback
+from traceloop.sdk import Traceloop
+from traceloop.sdk.tracing import tracing as Tracer
+from traceloop.sdk.decorators import workflow, task, agent
+from dotenv import load_dotenv, find_dotenv
+import pandas as pd
+import streamlit as st
+
+# Load the .env file
+if not load_dotenv(find_dotenv(),override=True):
+    raise Exception("Couldn't load .env file")
+
+#Add Telemetry
+TRACELOOP_API_KEY=os.getenv('TRACELOOP_API_KEY')
+Traceloop.init(app_name="Bike Recommendation App", disable_batch=True)
+# Generate a UUID
+uuid_obj = str(uuid.uuid4())
+Tracer.set_correlation_id(uuid_obj)
+
+#declare constant
+ASTRA_DB_SECURE_BUNDLE_PATH=os.getenv('ASTRA_SECUREBUNDLE_PATH')
+ASTRA_DB_APPLICATION_TOKEN=os.getenv('ASTRA_DB_TOKEN')
+ASTRA_DB_KEYSPACE=os.getenv('ASTRA_KEYSPACE')
+k=os.getenv('LIMIT_TOP_K')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+model_id = "text-embedding-ada-002"
+
+@task(name="Create Cassandra Connection")
+def create_connection():
+    #Establish Connectivity
+    st.write("Establishing AstraDB Connection...")
+    cluster = Cluster(
+    cloud={
+        "secure_connect_bundle": ASTRA_DB_SECURE_BUNDLE_PATH,
+    },
+    auth_provider=PlainTextAuthProvider(
+        "token",
+        ASTRA_DB_APPLICATION_TOKEN,
+    ),
+    )
+    session = cluster.connect()
+    keyspace = ASTRA_DB_KEYSPACE
+    return session, keyspace
+
+@task(name="Embed Input Query")
+def embed_query(customer_input):
+    # Create embedding based on same model
+    st.write("Using OpenAI to Create Embeddings for Input Query...")
+    embedding = openai.Embedding.create(input=customer_input, model=model_id)['data'][0]['embedding']
+    return embedding
+
+@task(name="Build top k simple query")
+def build_simple_query(customer_input, keyspace, k):
+    st.write("Building Simple Database Query...")
+    embedding = embed_query(customer_input)
+    query = SimpleStatement(
+    f"""
+    SELECT *
+    FROM {keyspace}.bikes
+    ORDER BY description_embedding ANN OF {embedding} LIMIT {k};
+    """
+    )
+    return query
+
+@task(name="Build top k hybrid query")
+def build_hybrid_query(customer_input, keyspace, filter, k):
+    st.write("Building Hybrid Search Query...")
+    embedding = embed_query(customer_input)
+    hybrid_query = SimpleStatement(
+    f"""
+    SELECT *
+    FROM {keyspace}.bikes
+    WHERE type : '{filter}'
+    ORDER BY description_embedding ANN OF {embedding} LIMIT {k};
+    """
+    )
+    return hybrid_query
+
+@task(name="Perform ANN search on Astra DB")
+def query_astra_db(session, query):
+    st.write("Retrieving results from Astra DB...")
+    results = session.execute(query)
+    top_results = results._current_rows
+    bikes_results = pd.DataFrame(top_results)
+    return bikes_results
+
+@workflow(name="Bike Recommendation Demo UI")
+def execute_demo_ui():
+    ##################################
+    st.title('🚲 Bike Recommendation Engine')
+    st.write("These Bike Recommendations are based on sample dataset.")
+    with st.expander('**Scenario Details**'):
+        st.write("""
+    The Bike Review and Descriptions have been embedded using [OpenAI's Text Embedding API](https://beta.openai.com/docs/api-reference/text-embeddings),
+    and stored in DataStax Astra.
+
+    Based on the bike recommendation asked, this demo will:
+
+    1. Search the vector database for the best bike matches, using these reviews and descrition;
+    2. User have the ability to further instruct on what type of bike;
+
+    """)
+
+    query = st.text_input('Please Enter your Bike Question:')
+    
+    if query:
+        # Create a checkbox with the label "Show optional text input"
+        show_optional_filter = st.checkbox("Check to Enter Bike Type", key="show_optional_filter")
+        if show_optional_filter:
+            filter = st.text_input("Please Enter Bike Type: (e.g. Kids Bike or eBikes)")
+
+            if filter:
+                session, keyspace = create_connection()
+                db_query = build_hybrid_query(query, keyspace, filter, k)
+                bikes_results = query_astra_db(session, db_query)
+                if bikes_results.notnull:
+                    st.write(bikes_results)
+            else:
+                session, keyspace = create_connection()
+                db_query = build_simple_query(query, keyspace, k)
+                bikes_results = query_astra_db(session, db_query)
+                if bikes_results.notnull:
+                    st.write(bikes_results)
+
+#call main method
+execute_demo_ui()
---
+++ Astra_Hybrid_Search_Bike_Recommendation/load_embeddings.py
@@ -0,0 +1,112 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.query import dict_factory, SimpleStatement
+import openai, os, uuid, time, requests, traceback
+import pandas as pd
+from traceloop.sdk import Traceloop
+from traceloop.sdk.tracing import tracing as Tracer
+from traceloop.sdk.decorators import workflow, task, agent
+from dotenv import load_dotenv, find_dotenv
+
+# Load the .env file
+if not load_dotenv(find_dotenv(),override=True):
+    raise Exception("Couldn't load .env file")
+
+#Add Telemetry
+TRACELOOP_API_KEY=os.getenv('TRACELOOP_API_KEY')
+Traceloop.init(app_name="Bike Recommendation App", disable_batch=True)
+# Generate a UUID
+uuid_obj = str(uuid.uuid4())
+Tracer.set_correlation_id(uuid_obj)
+
+#declare constant
+ASTRA_DB_SECURE_BUNDLE_PATH=os.getenv('ASTRA_SECUREBUNDLE_PATH')
+ASTRA_DB_APPLICATION_TOKEN=os.getenv('ASTRA_DB_TOKEN')
+ASTRA_DB_KEYSPACE=os.getenv('ASTRA_KEYSPACE')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+model_id = "text-embedding-ada-002"
+
+@task(name="Create Cassandra Connection")
+def create_connection():
+    #Establish Connectivity
+    cluster = Cluster(
+    cloud={
+        "secure_connect_bundle": ASTRA_DB_SECURE_BUNDLE_PATH,
+    },
+    auth_provider=PlainTextAuthProvider(
+        "token",
+        ASTRA_DB_APPLICATION_TOKEN,
+    ),
+    )
+    session = cluster.connect()
+    keyspace = ASTRA_DB_KEYSPACE
+    return session, keyspace
+
+@task(name="Drop Cassandra Objects")
+def drop_db_objects(session, keyspace):
+    session.execute(f"""DROP INDEX IF EXISTS {keyspace}.descriptions_vector_idx""")
+    session.execute(f"""DROP INDEX IF EXISTS {keyspace}.type_idx_analyzer""")
+    session.execute(f"""DROP TABLE IF EXISTS {keyspace}.bikes""")
+
+@task(name="Create Cassandra Objects")
+def create_db_objects(session, keyspace):
+    session.execute(f"""CREATE TABLE IF NOT EXISTS {keyspace}.bikes
+                    (model text,
+                    brand text,
+                    type text,
+                    price decimal,
+                    description text,
+                    description_embedding vector<float, 1536>,
+                    PRIMARY KEY (brand,model))""")
+    session.execute(f"""CREATE CUSTOM INDEX IF NOT EXISTS descriptions_vector_idx ON {ASTRA_DB_KEYSPACE}.bikes (description_embedding) USING 'org.apache.cassandra.index.sai.StorageAttachedIndex' WITH OPTIONS = {{'similarity_function':'dot_product'}}""")
+    session.execute(f"""CREATE CUSTOM INDEX IF NOT EXISTS type_idx_analyzer ON {ASTRA_DB_KEYSPACE}.bikes (type) USING 'org.apache.cassandra.index.sai.StorageAttachedIndex' WITH OPTIONS = {{'index_analyzer': '{{"tokenizer" : {{"name" : "standard"}},"filters" : [{{"name" : "porterstem"}},{{"name" : "lowercase",	"args": {{}}}}]}}'}};""")
+
+@task(name="Create and load Embeddings")    
+def create_load_embeddings(bikes, session):
+   for id in bikes.index:
+      description = bikes['description'][id].replace(',', '\,')
+      description = description.replace('"', '\"')
+      
+      # Create Embedding for each bike row, save them to the database
+      full_chunk = bikes['description'][id]
+      embedding = openai.Embedding.create(input=full_chunk, model=model_id)['data'][0]['embedding']
+      query = SimpleStatement(f"""INSERT INTO bike_rec.bikes(model, brand, price, type, description, description_embedding) VALUES (%s, %s, %s, %s, %s, %s)""")
+     
+      # Create a try-catch block
+      try:
+         session.execute(query, (bikes['model'][id], bikes['brand'][id], bikes['price'][id], bikes['type'][id], description, embedding), trace=True)
+      except Exception as e:
+          # Log the exception
+          traceback.print_exc()
+          print(e)
+          break
+
+def load_data_file():
+    #load data from sample json file
+    url = "https://raw.githubusercontent.com/mangatrai/vector-db-examples/main/Astra_Hybrid_Search_Bike_Recommendation/bikes.json"
+    response = requests.get(url)
+    bikes = response.json()
+    #print(bikes)
+    bikes = pd.DataFrame(bikes)
+    return bikes
+
+
+@workflow(name="Load Bike Recommendation Data")
+def run_loading_data():
+    #Create Connection
+    session, keyspace = create_connection()
+    
+    #database operations
+    drop_db_objects(session, keyspace)
+    create_db_objects(session,keyspace)
+
+    bikes = load_data_file()
+    
+    #call embedding function and load data
+    create_load_embeddings(bikes, session)
+
+run_loading_data()
+
+
+
+
